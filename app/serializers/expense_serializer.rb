e4f4758388e79fe4836bf82d513e5691043e1b3f
Alex Kovshovik <alex@shovik.com>
2016-04-28 20:16:09 -0400

    Fix nasty bug in expenses API

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,5 +1,9 @@
 class ExpenseSerializer < ActiveModel::Serializer
-  attributes :id, :budget_id, :amount, :comment, :created_at, :errors
+  attributes :id, :budget_id, :budget_name, :amount, :comment, :created_at, :errors
+
+  def budget_name
+    object.budget.name
+  end
 
   def errors
     object.errors.full_messages.join(', ')
