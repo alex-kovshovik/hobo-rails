d8a5b24916cf5d7b595bd1d1c3fce9061f7e2617
Alex Kovshovik <alex@shovik.com>
2016-05-02 20:42:54 -0400

    Fix capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:aspushkinus/hobo-rails.git'
 set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/srv/www/#{fetch(:application)}"
 
 # Default value for :scm is :git
 set :scm, :git
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,1 @@
-server '192.241.209.22', user: 'app', roles: %w(app db web)
+server '192.241.209.22:3422', user: 'app', roles: %w(app db web)
