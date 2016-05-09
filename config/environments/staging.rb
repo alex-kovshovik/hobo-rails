6c5767dcbec74bbf8395fd6fb1f9ec3698effcc2
Alex Kovshovik <alex@shovik.com>
2016-05-09 09:34:48 -0400

    Add staging environment support

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'hoboapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'staging.hoboapp.com', port: 443 }
 end
